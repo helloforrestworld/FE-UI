eb05a3ed58a26ce0bb62113237611ea845eb6679
helloforrestworld <dongweijia238@gmail.com>
2018-11-11 01:04:20 +0800

    添加button图标

--- src/button.vue
+++ src/button.vue
@@ -1,18 +1,36 @@
 <template>
-  <button class="fe-button">按钮</button>
+  <button class="fe-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#icon-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-  name: 'fe-button'
+  name: 'fe-button',
+  props: {
+    icon: String,
+    iconPosition: {
+      type: String,
+      default: 'left'
+    }
+  }
 }
 </script>
 
 <style lang="less">
 .fe-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   font-size: var(--font-size);
   height: var(--button-height);
-  padding: 0 1em;
+  padding: 0 0.8em;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
@@ -24,6 +42,23 @@ export default {
   }
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .3em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.3em;
+    }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
