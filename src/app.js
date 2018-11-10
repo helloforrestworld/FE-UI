f713227466f875c71ba57ffbcd7a709cdffeb5a8
helloforrestworld <dongweijia238@gmail.com>
2018-11-11 02:04:34 +0800

    完成button-group 创建

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import buttonGroup from './button-group'
 import Button from './button'
 import icon from './icon'
 
 Vue.component('fe-button', Button)
+Vue.component('fe-button-group', buttonGroup)
 Vue.component('fe-icon', icon)
 
 new Vue({
