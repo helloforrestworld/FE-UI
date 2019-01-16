73353572cec008d54195a21411fa9e58e586130f
helloforrestworld@163.com <helloforrestworld@163.com>
2019-01-17 01:19:50 +0800

    input组件

---
+++ src/input.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input value="" type="text" placeholder="" placeholder-class=""/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fe-input'
+}
+</script>
+
+<style lang="less">
+
+</style>
