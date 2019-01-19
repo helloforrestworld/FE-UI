cd25eac07248ea3c2ed6ca3766e677f8f29853f6
helloforrestworld@163.com <helloforrestworld@163.com>
2019-01-19 17:36:42 +0800

    input 事件测试

--- tests/input.test.js
+++ tests/input.test.js
@@ -10,37 +10,100 @@ describe('input.vue', () => {
     expect(Input).to.exist
   })
 
-  it('设置value.', () => {
-    const wrapper = mount(Input, {
-      propsData: {
-        value: '1'
-      }
+  // 测试input属性
+  describe('属性', () => {
+    it('设置value.', () => {
+      const wrapper = mount(Input, {
+        propsData: {
+          value: '1'
+        }
+      })
+      const inputElement = wrapper.find('input')
+      expect(inputElement.element.value).to.equal('1')
     })
-    const inputElement = wrapper.find('input')
-    expect(inputElement.element.value).to.equal('1')
+    it('设置disabled.', () => {
+      const wrapper = mount(Input, {
+        propsData: {
+          disabled: true
+        }
+      })
+      const inputElement = wrapper.find('input')
+      expect(inputElement.element.getAttribute('disabled')).to.equal('disabled')
+    })
+    it('设置error.', () => {
+      const wrapper = mount(Input, {
+        propsData: {
+          error: '错误'
+        }
+      })
+      const userElement = wrapper.find('use')
+      const mesElement = wrapper.find('.error-msg')
+      expect(userElement.element.getAttribute('xlink:href')).to.equal('#i-error')
+      expect(mesElement.element.innerHTML).to.equal('错误')
+    })
   })
 
-  it('设置disabled.', () => {
-    const wrapper = mount(Input, {
-      propsData: {
-        disabled: false
-      }
+  // 测试input事件
+  describe('事件', () => {
+    it('change事件.', () => {
+      const wrapper = mount(Input, {
+        propsData: {
+          error: '错误'
+        }
+      })
+      const callback = sinon.fake()
+      wrapper.vm.$on('change', callback)
+
+      // 触发chang事件
+      let event = new Event('change')
+      let inputElement = wrapper.find('input').element
+      inputElement.dispatchEvent(event)
+      expect(callback).to.have.been.calledWith('', event)
     })
-    const inputElement = wrapper.find('input')
-    expect(inputElement.element.getAttribute('disabled')).to.equal(true)
-  })
+    it('input事件.', () => {
+      const wrapper = mount(Input, {
+        propsData: {
+          error: '错误'
+        }
+      })
+      const callback = sinon.fake()
+      wrapper.vm.$on('input', callback)
 
-  it('设置error.', () => {
-    const wrapper = mount(Input, {
-      propsData: {
-        error: '错误'
-      }
+      // 触发chang事件
+      let event = new Event('input')
+      let inputElement = wrapper.find('input').element
+      inputElement.dispatchEvent(event)
+      expect(callback).to.have.been.calledWith('', event)
     })
-    const userElement = wrapper.find('use')
-    const mesElement = wrapper.find('.error-msg')
+    it('blur事件.', () => {
+      const wrapper = mount(Input, {
+        propsData: {
+          error: '错误'
+        }
+      })
+      const callback = sinon.fake()
+      wrapper.vm.$on('blur', callback)
 
+      // 触发chang事件
+      let event = new Event('blur')
+      let inputElement = wrapper.find('input').element
+      inputElement.dispatchEvent(event)
+      expect(callback).to.have.been.calledWith('', event)
+    })
+    it('focus事件.', () => {
+      const wrapper = mount(Input, {
+        propsData: {
+          error: '错误'
+        }
+      })
+      const callback = sinon.fake()
+      wrapper.vm.$on('focus', callback)
 
-    expect(userElement.element.getAttribute('xlink:href')).to.equal('#i-error')
-    expect(mesElement.element.innerHTML).to.equal('错误')
+      // 触发chang事件
+      let event = new Event('focus')
+      let inputElement = wrapper.find('input').element
+      inputElement.dispatchEvent(event)
+      expect(callback).to.have.been.calledWith('', event)
+    })
   })
 })
