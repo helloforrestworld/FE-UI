b441bb5e7cbc5a0c20e134b850b9e51a24c8b51d
helloforrestworld@163.com <helloforrestworld@163.com>
2019-02-17 21:27:20 +0800

    Toast加入动画

--- src/toast.vue
+++ src/toast.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="fe-toast" ref="wrapper" :class="toastClass">
-    <div class="fe-message" v-if="enableHtml" v-html="$slots.default[0]"></div>
-    <div class="fe-message" v-else>{{$slots.default[0]}}</div>
-    <div class="fe-toast-line" ref="line"></div>
-    <span class="fe-toast-close" v-if="this.showClose" @click="handleCloseClick">
-      {{closeBtn.text}}
-    </span>
+  <div class="fe-toast-wrapper" :class="toastClass">
+    <div class="fe-toast" ref="wrapper">
+      <div class="fe-message" v-if="enableHtml" v-html="$slots.default[0]"></div>
+      <div class="fe-message" v-else>{{$slots.default[0]}}</div>
+      <div class="fe-toast-line" ref="line"></div>
+      <span class="fe-toast-close" v-if="this.showClose" @click="handleCloseClick">
+        {{closeBtn.text}}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -87,45 +89,52 @@ export default {
 @toast-line-height: 1.8;
 @toast-font-color: #fff;
 @toast-bg: rgba(0, 0, 0, 0.75);
-.fe-toast {
-  min-height: @toast-height;
-  color: @toast-font-color;
-  font-size: @toast-font-size;
-  line-height: @toast-line-height;
-  background: @toast-bg;
-  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
-  border-radius: 4px;
-  display: flex;
-  align-items: center;
+@keyframes slide-top {
+  0% {transform: translateY(-100%);}
+  100% {transform: translateY(0);}
+}
+@keyframes slide-bottom {
+  0% {transform: translateY(100%);}
+  100% {transform: translateY(0);}
+}
+@keyframes fade-in {
+  0% {opacity: 0;}
+  100% {opacity: 1;}
+}
+.fe-toast-wrapper {
   position: fixed;
   left: 50%;
-  padding: 0 16px;
+  @animation-duration: 0.3s;
   &.position-top {
     top: 0;
     transform: translateX(-50%);
+    .fe-toast {
+      border-top-left-radius: 0;
+      border-top-right-radius: 0;
+      animation: slide-top @animation-duration;
+    }
   }
   &.position-bottom {
     bottom: 0;
     transform: translateX(-50%);
+    .fe-toast {
+      border-bottom-left-radius: 0;
+      border-bottom-right-radius: 0;
+      animation: slide-bottom @animation-duration;
+    }
   }
   &.position-middle {
     top: 50%;
     transform: translate(-50%, -50%);
+    .fe-toast {
+      animation: fade-in @animation-duration;
+    }
   }
-  .fe-message {
-    padding: 6px 0;
-  }
-  .fe-toast-line {
-    flex-shrink: 0;
-    margin-left: 16px;
-    width: 1px;
-    background: #666;
-  }
-  .fe-toast-close {
-    flex-shrink: 0;
-    cursor: pointer;
-    position: relative;
-    margin-left: 16px;
-  }
+}
+.fe-toast {
+  min-height: @toast-height;color: @toast-font-color;font-size: @toast-font-size;line-height: @toast-line-height;background: @toast-bg;box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);border-radius: 4px;display: flex;align-items: center;padding: 0 16px;
+  .fe-message {padding: 6px 0;}
+  .fe-toast-line {flex-shrink: 0;margin-left: 16px;width: 1px;background: #666;}
+  .fe-toast-close {flex-shrink: 0;cursor: pointer;position: relative;margin-left: 16px;}
 }
 </style>
